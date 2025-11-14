0247bc874e71848b23652729cf92d0cc001f99cc
Nikhil Pathak <nikhilpathak2602@gmail.com>
2025-11-14 13:16:42 +0530

    Draggable Text Content

--- script.js
+++ script.js
@@ -2,26 +2,32 @@ const data = [
   {
     id: 0,
     image: "./images/peach.jpg",
+    texts: ["Fresh Peach", "Sweet & Juicy"],
   },
   {
     id: 1,
     image: "./images/table.png",
+    texts: ["Dining Table", "Wooden & Elegant"],
   },
   {
     id: 2,
     image: "./images/shoe.png",
+    texts: ["Running Shoe", "Comfortable Fit"],
   },
   {
     id: 3,
     image: "./images/cone.png",
+    texts: ["Ice Cream Cone", "Delicious Dessert"],
   },
   {
     id: 4,
     image: "./images/forest.png",
+    texts: ["Mystic Forest", "Nature's Beauty"],
   },
   {
     id: 5,
     image: "./images/telephone.jpg",
+    texts: ["Vintage Phone", "Classic Design"],
   },
 ];
 
@@ -31,13 +37,58 @@ const Swiper_Wrapper = document.querySelector(".swiper-wrapper");
 
 data.map((item) => {
   const SlideElement = `<div class="swiper-slide">
-  <img src="${item.image}" class="slide-image"/>
+       <img src="${item.image}" class="slide-image"/>
+      <div class="slide-texts-container">
+        ${item.texts
+          .map((text) => `<div class="slide-text">${text}</div>`)
+          .join("")}
+      </div>
     </div>`;
   Swiper_Wrapper.innerHTML += SlideElement;
 });
 
+const Slide_Texts = document.querySelectorAll(".slide-text");
+Slide_Texts.forEach((item) => {
+  let offsetX = 0;
+  let offsetY = 0;
+  let isDragging = false;
+
+  item.addEventListener("click", (e) => {
+    isDragging = !isDragging;
+    if (isDragging) {
+      offsetX = e.clientX - item.offsetLeft;
+      offsetY = e.clientY - item.offsetTop;
+      item.style.cursor = "grabbing";
+    } else {
+      item.style.cursor = "pointer";
+    }
+  });
+
+  item.addEventListener("mousedown", (e) => {
+    isDragging = true;
+    offsetX = e.clientX - item.offsetLeft;
+    offsetY = e.clientY - item.offsetTop;
+    item.style.cursor = "grabbing";
+  });
+
+  document.addEventListener("mouseup", () => {
+    if (isDragging) {
+      isDragging = false;
+      item.style.cursor = "grab";
+    }
+  });
+
+  document.addEventListener("mousemove", (e) => {
+    if (isDragging) {
+      e.stopPropagation();
+      item.style.position = "absolute";
+      item.style.left = `${e.clientX - offsetX}px`;
+      item.style.top = `${e.clientY - offsetY}px`;
+    }
+  });
+});
+
 const swiper = new Swiper(".swiper", {
-  loop: true,
   pagination: {
     el: ".swiper-pagination",
   },
